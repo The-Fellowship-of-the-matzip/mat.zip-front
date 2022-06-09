f2fe8311c860a33fcb1b7a2ed78df7079a3d6c95
Yo Wook Kim <uk960214@gmail.com>
2022-06-10 00:06:00 +0900

    feat: 검색창 구현 및 스토리 작성

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const SearchInput = styled.input`
+  padding: 0.5em 1em;
+
+  background-color: ${({ theme }) => theme.white};
+
+  border: 1px solid ${({ theme }) => theme.secondary};
+  border-radius: 5px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+`;
+
+const Button = styled.button`
+  padding: 0 0.5em;
+
+  background-color: ${({ theme }) => theme.white};
+
+  border: 1px solid ${({ theme }) => theme.secondary};
+  border-radius: 5px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+`;
+
+function SearchBar() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSearchInput: React.ChangeEventHandler<HTMLInputElement> = ({
+    target: { value },
+  }) => {
+    setKeyword(value);
+  };
+
+  return (
+    <Container>
+      <SearchInput
+        type="text"
+        placeholder="검색"
+        value={keyword}
+        onChange={handleSearchInput}
+      />
+      <Button>🔍</Button>
+    </Container>
+  );
+}
+
+export default SearchBar;
