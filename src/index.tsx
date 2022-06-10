29eba0f4c880a487c295b29841805f318c7fe777
Yo Wook Kim <uk960214@gmail.com>
2022-06-10 16:33:57 +0900

    Revert "Issue1 상단 Navbar 구현"

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./style/Theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
