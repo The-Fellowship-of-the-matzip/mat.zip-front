d94df6a7b4de3393e2a6925f180c5174d3c886e2
Yo Wook Kim <uk960214@gmail.com>
2022-06-10 16:45:51 +0900

    Revert "Revert "Issue1 상단 Navbar 구현""

---
+++ src/stories/Header.stories.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default {
+  title: "Component/Header",
+  component: Header,
+};
+
+const Template = (args) => <Header />;
+
+export const Default = Template.bind({});
