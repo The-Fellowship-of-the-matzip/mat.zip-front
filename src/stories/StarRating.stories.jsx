9aa11c5fe2d402022eff39162047c741caac8fea
Yo Wook Kim <uk960214@gmail.com>
2022-06-11 13:32:28 +0900

    feat: 별점 컴포넌트 구현 및 스토리 작성

---
+++ src/stories/StarRating.stories.jsx
@@ -0,0 +1,10 @@
+import StarRating from "../components/StarRating";
+
+export default {
+  title: "Component/StarRating",
+  component: StarRating,
+};
+
+const Template = (args) => <StarRating />;
+
+export const Default = Template.bind({});
