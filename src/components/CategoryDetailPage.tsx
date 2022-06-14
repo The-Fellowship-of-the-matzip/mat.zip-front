e4489b59f7c6f4ed932f36e56b7be2e78883a050
Yo Wook Kim <uk960214@gmail.com>
2022-06-14 21:18:07 +0900

    feat: 가게 정보 리스트에 무한 스크롤 적용

--- src/components/CategoryDetailPage.tsx
+++ src/components/CategoryDetailPage.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import styled from "styled-components";
 import { MdArrowBackIos } from "react-icons/md";
 
-import type { StoreListProps } from "./StoreList";
-
 import Chip from "./Chip";
 import SectionHeader from "./SectionHeader";
 import StoreList from "./StoreList";
 import { useNavigate } from "react-router-dom";
 import InfiniteScroll from "./InfiniteScroll";
+import { stores } from "../mock/data";
 
 interface CategoryDetailPageProps {
   categoryName: string;
@@ -27,63 +26,6 @@ const ChipContainer = styled.div`
   justify-content: flex-end;
   gap: 1em;
 `;
-
-const stores: StoreListProps["stores"] = [
-  {
-    storeId: 1,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "냠냠 치킨",
-    campus: "잠실",
-    distance: 0.5,
-    starCount: 3,
-  },
-  {
-    storeId: 2,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "욤욤 치킨",
-    campus: "잠실",
-    distance: 0.2,
-    starCount: 4,
-  },
-  {
-    storeId: 3,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "념념 치킨",
-    campus: "잠실",
-    distance: 1,
-    starCount: 1,
-  },
-  {
-    storeId: 4,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "냠냠 치킨",
-    campus: "잠실",
-    distance: 0.5,
-    starCount: 3,
-  },
-  {
-    storeId: 5,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "욤욤 치킨",
-    campus: "잠실",
-    distance: 0.2,
-    starCount: 4,
-  },
-  {
-    storeId: 6,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "념념 치킨",
-    campus: "잠실",
-    distance: 1,
-    starCount: 1,
-  },
-];
 
 function CategoryDetailPage({ categoryName }: CategoryDetailPageProps) {
   const [isSelected, setIsSelected] = useState({
--- src/components/CategoryPage.tsx
+++ src/components/CategoryPage.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
-import type { StoreListProps } from "./StoreList";
-
 import Category from "./Category";
 import SectionHeader from "./SectionHeader";
 import StoreList from "./StoreList";
+import { stores } from "../mock/data";
+import InfiniteScroll from "./InfiniteScroll";
+import { useState } from "react";
 
 const CategoryPageContainer = styled.div`
   padding: 1em;
@@ -26,37 +27,13 @@ const categories = [
   { categoryId: 8, desc: "기타" },
 ];
 
-const stores: StoreListProps["stores"] = [
-  {
-    storeId: 1,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "냠냠 치킨",
-    campus: "잠실",
-    distance: 0.5,
-    starCount: 3,
-  },
-  {
-    storeId: 2,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "욤욤 치킨",
-    campus: "잠실",
-    distance: 0.2,
-    starCount: 4,
-  },
-  {
-    storeId: 3,
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    name: "념념 치킨",
-    campus: "잠실",
-    distance: 1,
-    starCount: 1,
-  },
-];
-
 function CategoryPage() {
+  const [data, setData] = useState([...stores]);
+
+  const loadMoreStores = () => {
+    setData((prevData) => [...prevData, ...stores]);
+  };
+
   return (
     <CategoryPageContainer>
       <section>
@@ -65,7 +42,9 @@ function CategoryPage() {
       </section>
       <section>
         <SectionHeader>이런 메뉴는 어떤가요?</SectionHeader>
-        <StoreList stores={stores} />
+        <InfiniteScroll handleContentLoad={loadMoreStores} hasMore={true}>
+          <StoreList stores={data} />
+        </InfiniteScroll>
       </section>
     </CategoryPageContainer>
   );
