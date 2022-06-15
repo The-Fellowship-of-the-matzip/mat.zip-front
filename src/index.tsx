79962549409d3d6b613f64b3da097699bd4b5703
JASUN LEE <liswktjs@gmail.com>
2022-06-15 12:05:15 +0900

    style: style 기본 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./style/Theme";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "./style/Theme";
+
 import GlobalStyle from "./style/GlobalStyle";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
