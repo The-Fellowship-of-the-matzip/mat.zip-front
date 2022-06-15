863bad40fdf2226f43b7a79d69358c2f5feeb485
Yo Wook Kim <uk960214@gmail.com>
2022-06-15 15:47:27 +0900

    refactor: index.tsx import 정리 및 prettier 설정 수정
    Co-authored-by: JASUN LEE <liswktjs@users.noreply.github.com>
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import App from "./App";
-import GlobalStyle from "./style/GlobalStyle";
-import { theme } from "./style/Theme";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
+
+import App from "App";
+
+import GlobalStyle from "style/GlobalStyle";
+import { theme } from "style/Theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
