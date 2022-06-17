9657d560bae9528c7b6a8718d81dd29e5a819e88
Yo Wook Kim <uk960214@gmail.com>
2022-06-17 15:58:54 +0900

    feat: 선택한 캠퍼스를 로컬 스토리지에 저장하도록 구현

--- src/context/CampusContextProvider.tsx
+++ src/context/CampusContextProvider.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+
+import useStoredState from "hooks/useStoredState";
 
 export type Campus = "잠실" | "선릉";
 type CampusContext = Campus | null;
 
-type SetCampusContext = React.Dispatch<
-  React.SetStateAction<CampusContext | null>
->;
+type SetCampusContext = (value: CampusContext) => void;
 
 export const campusContext = React.createContext<null | CampusContext>(null);
 export const setCampusContext = React.createContext<SetCampusContext>(() => {});
 
 function CampusContextProvider({ children }: React.PropsWithChildren<{}>) {
-  const [campus, setCampus] = useState<CampusContext>(null);
+  const [campus, setCampus] = useStoredState<CampusContext>("campus", null);
 
   return (
     <campusContext.Provider value={campus}>
