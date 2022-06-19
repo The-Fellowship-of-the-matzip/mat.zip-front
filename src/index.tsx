adbf905712e4984090a09cfafd1a855898ab18ed
Yo Wook Kim <uk960214@gmail.com>
2022-06-19 23:11:29 +0900

    fix: react query provider 추가
    Co-authored-by: JASUN LEE <liswktjs@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import CampusContextProvider from "context/CampusContextProvider";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
@@ -18,15 +19,20 @@ if (process.env.NODE_ENV === "development") {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <CampusContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </CampusContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <CampusContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </CampusContextProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
