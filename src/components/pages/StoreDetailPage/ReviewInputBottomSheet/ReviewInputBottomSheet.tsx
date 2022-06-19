0c97c20e0fb15cbcaf8dbabfaad01df69562a717
JASUN LEE <liswktjs@gmail.com>
2022-06-19 17:04:50 +0900

    feat: 리뷰 post 요청

--- src/components/pages/StoreDetailPage/ReviewInputBottomSheet/ReviewInputBottomSheet.tsx
+++ src/components/pages/StoreDetailPage/ReviewInputBottomSheet/ReviewInputBottomSheet.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import { useMutation } from "react-query";
 
 import BottomSheet from "components/common/BottomSheet/BottomSheet";
 import StarRating from "components/common/StarRating/StarRating";
@@ -13,15 +15,41 @@ type Props = {
 const DEFAULT_RATING = 4;
 
 function ReviewInputBottomSheet({ closeSheet, onSubmit }: Props) {
+  // TODO: 레스토랑 아이디를 받아와야 함
+  // TODO: onSubmit 함수의 위치
+  const restaurantId = 0;
+  const mutation = useMutation((newReview) => {
+    const accessToken = localStorage.getItem("accessToken");
+    return axios.post(`/api/restaurants/${restaurantId}/reviews`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  });
+
   const [rating, setRating] = useState(DEFAULT_RATING);
+  const [reviewContent, setReviewContent] = useState("");
+  const [menuInput, setMenuInput] = useState("");
+
+  const handleSubmitRequest = () => {
+    mutation.mutate({ content: reviewContent, score: rating, menu: menuInput });
+  };
 
   return (
     <BottomSheet title={"리뷰 남기기"} closeSheet={closeSheet}>
-      <S.Form onSubmit={onSubmit}>
+      <S.Form onSubmit={handleSubmitRequest}>
         <S.Label htmlFor="menu-input">메뉴 입력</S.Label>
-        <S.MenuInput id="menu-input" />
+        <S.MenuInput
+          id="menu-input"
+          value={menuInput}
+          onChange={(e) => setMenuInput(e.target.value)}
+        />
         <S.Label htmlFor="review">총평</S.Label>
-        <S.ReviewTextArea id="review" />
+        <S.ReviewTextArea
+          id="review"
+          value={reviewContent}
+          onChange={(e) => setReviewContent(e.target.value)}
+        />
         <S.BottomWrapper>
           <StarRating rating={rating} setRating={setRating} />
           <S.SubmitButton>제출</S.SubmitButton>
