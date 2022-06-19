c2c22fe16316d82218a8f38c858b8e6e054d24a0
JASUN LEE <liswktjs@gmail.com>
2022-06-19 16:32:16 +0900

    feat: 음식 항목별 조회 하기

--- src/components/pages/CategoryDetailPage/index.tsx
+++ src/components/pages/CategoryDetailPage/index.tsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import axios from "axios";
+import { campusContext } from "context/CampusContextProvider";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { MdArrowBackIos } from "react-icons/md";
+import { useInfiniteQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 
 import Chip from "components/common/Chip/Chip";
@@ -16,16 +20,61 @@ interface CategoryDetailPageProps {
 }
 
 function CategoryDetailPage({ categoryName }: CategoryDetailPageProps) {
+  const navigate = useNavigate();
+  const campusName = useContext(campusContext);
+  const campusId = campusName === "잠실" ? 1 : 2;
+  const categoryId = 1;
+
+  const [pageParam, setPageParam] = useState(0);
+
   const [isSelected, setIsSelected] = useState({
     starOrder: false,
     abcOrder: false,
   });
-  const [data, setData] = useState([...stores]);
 
-  const navigate = useNavigate();
+  // 기본을 score 순서로 처음 조회
+  const fetchCategoryStoreList = async ({ pageParam = 0 }) => {
+    let filterName = "score";
+    if (isSelected.abcOrder) {
+      filterName = "spell";
+    }
+    const data = await axios.get(`
+    /api/campuses/${campusId}/restaurants?categoryId=${categoryId}&filter=${filterName}page=${pageParam}&size=${10}
+    `);
+    return data;
+  };
+
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    fetchNextPage,
+    isFetching,
+    refetch,
+  } = useInfiniteQuery("categoryStore", fetchCategoryStoreList, {
+    getNextPageParam: (lastPage) => {
+      if (lastPage.hasNext) {
+        setPageParam(pageParam + 1);
+        return pageParam + 1;
+      }
+      setPageParam(0);
+      return 0;
+    },
+  });
+
+  // const [data, setData] = useState([...stores]);
+
+  // isSelected 선택 된 값이 달라질때마다 refetch 해서 데이터 업데이트
+  useEffect(() => {
+    refetch();
+  }, [isSelected, refetch]);
 
   const loadMoreStores = () => {
-    setData((prevData) => [...prevData, ...stores]);
+    if (data.hasNext) {
+      fetchNextPage();
+    }
+    return;
   };
 
   // TODO: 클릭한 값에 따라 stores prop 바꾸기
@@ -67,6 +116,9 @@ function CategoryDetailPage({ categoryName }: CategoryDetailPageProps) {
         </Chip>
       </S.ChipContainer>
       <InfiniteScroll handleContentLoad={loadMoreStores} hasMore={true}>
+        {isLoading && <div>로딩중...</div>}
+        {isError && <div>{error}</div>}
+        {isFetching && <div>다음 페이지 로딩 중</div>}
         <StoreList stores={data} />
       </InfiniteScroll>
     </S.CategoryDetailPageContainer>
