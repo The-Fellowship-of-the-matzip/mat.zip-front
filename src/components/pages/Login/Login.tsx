6253f48503125de548584380af41a08b9fbeb122
Yo Wook Kim <uk960214@gmail.com>
2022-06-23 01:07:43 +0900

    refactor: exhaustive deps 경고 제거

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useEffect, useRef } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
