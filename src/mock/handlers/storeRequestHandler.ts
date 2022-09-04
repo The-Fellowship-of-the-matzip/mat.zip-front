54ddbe98f3d1a02f7bb0b3d24796974a231ad8a7
Yo Wook Kim <uk960214@gmail.com>
2022-09-04 14:22:49 +0900

    fix: 식당 추가 요청 조회 api 모킹에 페이징 추가

--- src/mock/handlers/storeRequestHandler.ts
+++ src/mock/handlers/storeRequestHandler.ts
@@ -8,7 +8,18 @@ export const storeRequestHandler = [
   rest.get(
     `${API_BASE_URL}/campuses/:campusId/restaurants/requests`,
     (req, res, ctx) => {
-      const response = { hasNext: false, items: storeRequests };
+      const page = req.url.searchParams.get("page") || 0;
+      const size = req.url.searchParams.get("size");
+
+      const sizeNo = Number(size); // 1, ...
+      const pageNo = Number(page); // 0, ...
+      const startIndex = pageNo * sizeNo;
+      const endIndex = startIndex + sizeNo;
+
+      const response = {
+        hasNext: endIndex < storeRequests.length,
+        items: storeRequests.slice(startIndex, endIndex),
+      };
       return res(ctx.status(200), ctx.json(response));
     }
   ),
