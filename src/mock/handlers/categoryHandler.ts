dfd4938963bafb0c6f6ae66a0707edcccaf1086a
Yo Wook Kim <uk960214@gmail.com>
2022-09-04 13:22:37 +0900

    fix: 카테고리 모킹 핸들러 url 주소 수정

--- src/mock/handlers/categoryHandler.ts
+++ src/mock/handlers/categoryHandler.ts
@@ -5,7 +5,7 @@ import { API_BASE_URL } from "constants/api";
 import { categories } from "mock/data";
 
 export const categoryHandler = [
-  rest.get(`${API_BASE_URL}/api/categories`, (req, res, ctx) => {
+  rest.get(`${API_BASE_URL}/categories`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(categories));
   }),
 ];
