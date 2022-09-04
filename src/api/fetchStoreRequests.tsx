0865bff3eedae5c912b0c66c553585574655cfcd
Yo Wook Kim <uk960214@gmail.com>
2022-09-04 17:20:19 +0900

    fix: api 응답 키값 변화에 따른 수정

--- src/api/fetchStoreRequests.tsx
+++ src/api/fetchStoreRequests.tsx
@@ -12,13 +12,38 @@ interface Params {
   queryKey: any;
 }
 
+interface StoreRequestGetResponse {
+  items: {
+    id: number;
+    categoryId: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+    name: string;
+    author: string;
+    updatable: boolean;
+    registered: boolean;
+  }[];
+  hasNext: boolean;
+}
+
 const fetchStoreRequests = async ({ pageParam = 0, queryKey }: Params) => {
   const [, { size, campusId }] = queryKey;
-  const { data } = await axiosInstance.get(ENDPOINTS.STORE_REQUESTS(campusId), {
-    params: { page: pageParam, size },
-  });
+  const { data } = await axiosInstance.get<StoreRequestGetResponse>(
+    ENDPOINTS.STORE_REQUESTS(campusId),
+    {
+      params: { page: pageParam, size },
+    }
+  );
 
-  return { ...data, nextPageParam: pageParam + 1 };
+  const parsedData = {
+    items: data.items.map(({ id, updatable, registered, ...rest }) => ({
+      id: String(id),
+      isAuthor: updatable,
+      isRegistered: registered,
+      ...rest,
+    })),
+    hasNext: data.hasNext,
+  };
+
+  return { ...parsedData, nextPageParam: pageParam + 1 };
 };
 
 export default fetchStoreRequests;
