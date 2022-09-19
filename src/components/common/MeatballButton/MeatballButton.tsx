83aadf8a743d8de8ffa6495eb86c0a9c2f36d0c6
JASUN LEE <liswktjs@gmail.com>
2022-09-19 15:08:21 +0900

    style: meatBall Dot Button react-icons으로 교체

--- src/components/common/MeatballButton/MeatballButton.tsx
+++ src/components/common/MeatballButton/MeatballButton.tsx
@@ -1,23 +1,11 @@
+import { BiDotsHorizontal } from "react-icons/bi";
+
 import * as S from "components/common/MeatballButton/MeatballButton.style";
 
 export type MeatballButtonProps = {
   ariaLabel: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
-
-export const MeatballMenuSvg = () => (
-  <svg
-    stroke="currentColor"
-    fill="currentColor"
-    strokeWidth="0"
-    viewBox="0 0 20 20"
-    height="20"
-    width="20"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-  </svg>
-);
 
 const MeatballButton: React.FC<MeatballButtonProps> = ({
   ariaLabel,
@@ -29,7 +17,7 @@ const MeatballButton: React.FC<MeatballButtonProps> = ({
       type="button"
       onClick={onClick}
     >
-      <MeatballMenuSvg />
+      <BiDotsHorizontal size="20" />
     </S.MeatballMenuButton>
   );
 };
