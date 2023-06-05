5023531b1a07699fc4584aab4e09a50e9a1b5c62
ashleysyheo <ashleysyheo@gmail.com>
2023-06-05 15:53:21 +0900

    refactor: 변경된 api msw에 적용
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

--- src/mock/handlers/reviewHandler.ts
+++ src/mock/handlers/reviewHandler.ts
@@ -71,6 +71,8 @@ export const reviewHandler = [
           username: "김맛집",
           profileImage:
             "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+          reviewCount: 3,
+          averageRating: 3.5,
         },
         content,
         rating,
