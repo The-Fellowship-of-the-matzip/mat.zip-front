436caebb551be3be3a6391269191fa9054fd6472
ashleysyheo <ashleysyheo@gmail.com>
2023-06-12 01:00:52 +0900

    feat: Textarea 컴포넌트 생성

---
+++ src/components/common/Textarea/Textarea.tsx
@@ -0,0 +1,43 @@
+import Label from "../Label/Label";
+import * as S from "./Textarea.style";
+import type { ComponentPropsWithoutRef } from "react";
+
+import type { CSSProp } from "styled-components";
+
+export interface TextareaProps extends ComponentPropsWithoutRef<"textarea"> {
+  $size?: "small" | "medium" | "large";
+  label?: string;
+  supportingText?: string;
+  isError?: boolean;
+  css?: CSSProp;
+}
+
+function Textarea({
+  $size = "medium",
+  label,
+  supportingText,
+  isError = false,
+  id,
+  required = false,
+  ...attributes
+}: TextareaProps) {
+  return (
+    <>
+      {label && (
+        <Label id={id} required={required}>
+          {label}
+        </Label>
+      )}
+      <S.Textarea
+        id={id}
+        $size={$size}
+        isError={isError}
+        required={required}
+        {...attributes}
+      />
+      {supportingText && <S.SupportingText></S.SupportingText>}
+    </>
+  );
+}
+
+export default Textarea;
