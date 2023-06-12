98c4d7d4e6e2f1a89acc62f0d95e1c36afbf0713
ashleysyheo <ashleysyheo@gmail.com>
2023-06-12 15:33:39 +0900

    feat: x svg 아이콘 추가

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,3 +1,3 @@
+export { ReactComponent as CloseIcon } from "./close-icon.svg";
 export { ReactComponent as PlusIcon } from "./plus-icon.svg";
-
 export { ReactComponent as SearchIcon } from "./search-icon.svg";
