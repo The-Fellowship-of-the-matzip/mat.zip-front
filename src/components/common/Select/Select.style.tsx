10f4a29ed3b962b50b863c3a4dca2fe1c4276777
ashleysyheo <ashleysyheo@gmail.com>
2023-06-12 15:36:00 +0900

    feat: Select 컴포넌트 생성

---
+++ src/components/common/Select/Select.style.tsx
@@ -0,0 +1,62 @@
+import type { SelectProps } from "./Select";
+
+import styled, { css } from "styled-components";
+
+const getVariantStyling = (isError: Required<SelectProps>["isError"]) => {
+  const style = {
+    default: css`
+      background-color: ${({ theme }) => theme.color.gray100};
+      color: ${({ theme }) => theme.color.gray800};
+      border: 1px solid ${({ theme }) => theme.color.gray100};
+    `,
+    error: css`
+      background-color: ${({ theme }) => theme.color.redLight};
+      color: ${({ theme }) => theme.color.gray800};
+      border: 1px solid ${({ theme }) => theme.color.redDark};
+
+      &:focus {
+        border: 1px solid ${({ theme }) => theme.color.redDark};
+        box-shadow: 0 0 0 3px ${({ theme }) => theme.color.redLight1};
+      }
+    `,
+  };
+
+  return isError ? style.error : style.default;
+};
+
+const getSizeStyling = (size: Required<SelectProps>["$size"]) => {
+  const style = {
+    small: css`
+      padding: 0.8rem 1.2rem;
+      font-size: 1.4rem;
+    `,
+    medium: css`
+      padding: 1.4rem;
+      font-size: 1.6rem;
+    `,
+    large: css`
+      padding: 1.6rem 1.4rem;
+      font-size: 1.6rem;
+    `,
+  };
+
+  return style[size];
+};
+
+export const Select = styled.select<Omit<SelectProps, "children">>`
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  ${({ isError = false }) => getVariantStyling(isError)}
+  ${({ $size = "medium" }) => getSizeStyling($size)}
+  ${(props) => props.css}
+
+  &:focus {
+    outline: 0;
+  }
+`;
+
+export const SupportingText = styled.p<Omit<SelectProps, "children">>`
+  font-size: 1.4rem;
+  line-height: 2rem;
+  color: ${({ theme, isError = false }) =>
+    isError ? theme.color.redDark : theme.color.gray600};
+`;
