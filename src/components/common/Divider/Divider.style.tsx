42e241147dac7b444bd43e1397fceb3ed910e6de
ashleysyheo <ashleysyheo@gmail.com>
2023-06-13 01:34:45 +0900

    refactor: Divider styled component export 방식 변경

--- src/components/common/Divider/Divider.style.tsx
+++ src/components/common/Divider/Divider.style.tsx
@@ -2,9 +2,7 @@ import { DividerProps } from "./Divider";
 
 import styled from "styled-components";
 
-const Divider = styled.div<DividerProps>`
+export const Divider = styled.div<DividerProps>`
   border-bottom: ${({ thickness }) => thickness}px solid
     ${({ theme }) => theme.color.gray200};
 `;
-
-export { Divider };
