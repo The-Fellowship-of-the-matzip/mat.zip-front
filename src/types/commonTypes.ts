f37c0f2b61a3ba496ae4c7c8560dbdad9fcf1a89
hafnium1923 <hafnium1923@gmail.com>
2023-06-14 13:15:59 +0900

    refactor: mockData 파일에 있던 타입 별도 파일로 분리

---
+++ src/types/commonTypes.ts
@@ -0,0 +1,21 @@
+export interface Store {
+  id: number;
+  name: string;
+  address: string;
+  distance: number;
+  kakaoMapUrl: string;
+  imageUrl: string;
+  rating: number;
+}
+
+export interface Review {
+  id: number;
+  author: {
+    username: string;
+    profileImage: string;
+  };
+  content: string;
+  rating: number;
+  menu: string;
+  updatable: boolean;
+}
