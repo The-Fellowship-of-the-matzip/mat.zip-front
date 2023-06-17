e12e196f728952a1beb1613220635d2660e4caef
hafnium1923 <hafnium1923@gmail.com>
2023-06-17 21:58:34 +0900

    fix: import에서 발생하는 오류 해결

--- src/types/common/campusTypes.ts
+++ src/types/common/campusTypes.ts
@@ -1,4 +1,4 @@
-import CAMPUS from "constants/campus";
+import { CAMPUS } from "constants/campus";
 
 export type CampusId = typeof CAMPUS.JAMSIL.id | typeof CAMPUS.SEOULLEUNG.id;
 export type Campus = typeof CAMPUS.JAMSIL.name | typeof CAMPUS.SEOULLEUNG.name;
