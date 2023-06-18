b01bac98f4977c62bd309e31fb0cb61be68b5dc0
ashleysyheo <ashleysyheo@gmail.com>
2023-06-18 21:15:58 +0200

    feat: 카테고리 svg 아이콘 추가

---
+++ src/asset/categoryIcons.ts
@@ -0,0 +1,8 @@
+export { ReactComponent as KoreanCategoryIcon } from "./korean-category-icon.svg";
+export { ReactComponent as ChineseCategoryIcon } from "./chinese-category-icon.svg";
+export { ReactComponent as JapaneseCategoryIcon } from "./japanese-category-icon.svg";
+export { ReactComponent as WesternCategoryIcon } from "./western-category-icon.svg";
+export { ReactComponent as SaladCategoryIcon } from "./salad-category-icon.svg";
+export { ReactComponent as MeatCategoryIcon } from "./meat-category-icon.svg";
+export { ReactComponent as CafeCategoryIcon } from "./cafe-category-icon.svg";
+export { ReactComponent as AlcoholCategoryIcon } from "./alcohol-category-icon.svg";
