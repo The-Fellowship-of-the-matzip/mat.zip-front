795248b97d9c083aeb41670a7447f483b1a5653e
ashleysyheo <ashleysyheo@gmail.com>
2023-06-26 02:25:28 +0900

    feat: < svg 아이콘 추가
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as CloseIcon } from "./close-icon.svg";
 export { ReactComponent as LogoLight } from "./logo-light.svg";
 export { ReactComponent as PlusIcon } from "./plus-icon.svg";
 export { ReactComponent as SearchIcon } from "./search-icon.svg";
+export { ReactComponent as LeftIcon } from "./left-icon.svg";
