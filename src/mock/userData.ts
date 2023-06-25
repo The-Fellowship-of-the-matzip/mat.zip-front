38a0e89a571c345962c79216ce6c451cab798f8d
ashleysyheo <ashleysyheo@gmail.com>
2023-06-25 22:28:00 +0900

    feat: 사용자 작성 리뷰 조회 msw 및 목 데이터 추가
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

--- src/mock/userData.ts
+++ src/mock/userData.ts
@@ -1,3 +1,5 @@
+import type { UserReview } from "types/common";
+
 export const userProfile = {
   username: "huni",
   profileImage:
@@ -54,5 +56,99 @@ export const bookmarkedStores = [
     rating: 3,
     reviewCount: 3,
     saved: true,
+  },
+];
+
+export const userReviews: UserReview[] = [
+  {
+    id: 1,
+    restaurant: {
+      id: 2,
+      name: "맛있는 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "맛있어요",
+    rating: 4,
+    menu: "맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 2,
+    restaurant: {
+      id: 3,
+      name: "올로ㅗㄹ 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 3,
+    restaurant: {
+      id: 3,
+      name: "우에에ㅔㄱ 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 4,
+    restaurant: {
+      id: 3,
+      name: "냐아앙ㅁ 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 5,
+    restaurant: {
+      id: 3,
+      name: "뇨오옴 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 6,
+    restaurant: {
+      id: 3,
+      name: "룰랄룰 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
+  },
+  {
+    id: 7,
+    restaurant: {
+      id: 3,
+      name: "크아악 식당",
+      imageUrl:
+        "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    content: "더 맛있어요",
+    rating: 5,
+    menu: "더 맛있는 메뉴",
+    updatable: true,
   },
 ];
