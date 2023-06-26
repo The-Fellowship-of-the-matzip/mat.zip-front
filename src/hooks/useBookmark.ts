2a80ef303c5bd2e54d713a68f7900eea95c4e17f
ashleysyheo <ashleysyheo@gmail.com>
2023-06-26 17:52:01 +0900

    refactor: 북마크 저장/삭제 에러 처리 방법 수정
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

--- src/hooks/useBookmark.ts
+++ src/hooks/useBookmark.ts
@@ -8,16 +8,18 @@ export const useBookmark = (restaurantId: number, saved: boolean) => {
   const [bookmark, setBookmark] = useState(saved);
 
   const deleteBookmark = useMutation(sendBookmarkDeleteRequest(restaurantId), {
-    onError: () => {
+    onMutate: () => ({ prevBookmark: bookmark }),
+    onError: (err, _, context) => {
       alert("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-      window.location.reload();
+      setBookmark(context?.prevBookmark!);
     },
   });
 
   const postBookmark = useMutation(sendBookmarkPostRequest(restaurantId), {
-    onError: () => {
+    onMutate: () => ({ prevBookmark: bookmark }),
+    onError: (err, _, context) => {
       alert("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-      window.location.reload();
+      setBookmark(context?.prevBookmark!);
     },
   });
 
