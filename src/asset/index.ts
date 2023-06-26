5d4a6a8e3c56e825de87c1fa0488b73e822d2092
ashleysyheo <ashleysyheo@gmail.com>
2023-06-26 20:12:17 +0900

    feat: 지도 pin 아이콘들 추가

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -3,3 +3,6 @@ export { ReactComponent as LogoLight } from "./logo-light.svg";
 export { ReactComponent as PlusIcon } from "./plus-icon.svg";
 export { ReactComponent as SearchIcon } from "./search-icon.svg";
 export { ReactComponent as LeftIcon } from "./left-icon.svg";
+export { ReactComponent as PinIcon } from "./pin-icon.svg";
+export { ReactComponent as ClickedPinIcon } from "./clicked-pin-icon.svg";
+export { ReactComponent as CampusPinIcon } from "./campus-pin-icon.svg";
