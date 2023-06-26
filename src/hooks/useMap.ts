e6c211b1a82b7ecfa4d95b0952ddc28f86eb22a6
ashleysyheo <ashleysyheo@gmail.com>
2023-06-26 22:45:06 +0900

    feat: useMap 커스텀 훅 추가
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

---
+++ src/hooks/useMap.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { BookmarkStore, Store } from "types/common";
+
+export interface Position {
+  id: number;
+  latlng: {
+    lat: number;
+    lng: number;
+  };
+  addressName: string;
+}
+
+export const useMap = (stores: Store[] | BookmarkStore[]) => {
+  const [positions, setPositions] = useState<Position[]>([]);
+
+  const getPosition = (store: Store | BookmarkStore): Promise<Position> => {
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    return new Promise((resolve, reject) => {
+      geocoder.addressSearch(store.address, (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          resolve({
+            id: store.id,
+            latlng: {
+              lat: Number(result[0].y),
+              lng: Number(result[0].x),
+            },
+            addressName: result[0].address_name,
+          });
+        } else {
+          reject(new Error("없는 주소입니다."));
+        }
+      });
+    });
+  };
+
+  const getStorePositions = () => {
+    if (!stores) {
+      setPositions([]);
+
+      return;
+    }
+
+    const getAddressPromises = stores.map(getPosition);
+
+    Promise.all(getAddressPromises).then((results) => {
+      setPositions([...results]);
+    });
+  };
+
+  useEffect(() => {
+    getStorePositions();
+  }, [stores]);
+
+  return { positions };
+};
