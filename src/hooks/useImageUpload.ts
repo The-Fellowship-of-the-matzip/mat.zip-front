54b1c2850fe23a9a79f0fb427bd140dee992e83c
ashleysyheo <ashleysyheo@gmail.com>
2023-06-27 19:35:40 +0900

    refactor: 이미지 업로드 실패 메세지 수정

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -13,7 +13,7 @@ export const useImageUpload = (url: null | string = null) => {
       const { imageUrl } = await sendImageUploadPostRequest(image);
       setUploadedImageUrl(imageUrl);
     } catch (error) {
-      alert("이미지 업로드에 실패했습니다.");
+      alert("이미지 업로드에 실패했습니다. 다시 시도해 주세요.");
     }
   };
 
