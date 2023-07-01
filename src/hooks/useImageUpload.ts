d587de3f3061027e970cc4b70bf6b44492787c5b
ashleysyheo <ashleysyheo@gmail.com>
2023-07-02 01:26:56 +0900

    fix: 업로드한 이미지 삭제 후 같은 이미지 재업로드 안 되는 문제 해결

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -5,16 +5,20 @@ import sendImageUploadPostRequest from "api/image/sendImageUploadPostRequest";
 export const useImageUpload = (url: null | string = null) => {
   const [uploadedImageUrl, setUploadedImageUrl] = useState(url);
 
-  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
+  const handleImageUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!event.target.files) return;
 
     try {
-      const image = e.target.files[0];
+      const image = event.target.files[0];
       const { imageUrl } = await sendImageUploadPostRequest(image);
       setUploadedImageUrl(imageUrl);
     } catch (error) {
       alert("이미지 업로드에 실패했습니다. 다시 시도해 주세요.");
     }
+
+    event.target.value = "";
   };
 
   const handleImageRemoval = () => {
