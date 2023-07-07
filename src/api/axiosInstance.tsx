f42c534dca92389596ea47ad3773b7c41fe8d6ca
ashleysyheo <ashleysyheo@gmail.com>
2023-07-07 13:07:04 +0900

    feat: accessToken이 필요한 경우에 사용할 authAxiosInstance 생성 및 interceptor 추가

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,17 +1,22 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios from "axios";
 
-import { API_BASE_URL } from "constants/api";
+import { ACCESS_TOKEN, API_BASE_URL } from "constants/api";
+
+import { checkAndSetToken, handleAPIError } from "api/utils";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-const handleAPIError = (error: AxiosError) => {
-  const { data } = error.response as AxiosResponse;
-
-  throw Error(data.message);
-};
-
 axiosInstance.interceptors.response.use((response) => response, handleAPIError);
+
+export const authAxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
+});
+
+authAxiosInstance.interceptors.request.use(checkAndSetToken, handleAPIError);
 
 export default axiosInstance;
