d56cc146a652bc90f65856a2c70104dba84e6b55
손진영 <sm099@naver.com>
2024-06-21 22:05:11 +0900

    fix(BookmarkListPage): page 내에서 에러 처리하도록 변경
    fetcher에서 error throw 후 page 컴포넌트 내에서 error를 처리하도록 변경

--- src/components/pages/MyPage/BookmarkListPage/BookmarkListPage.tsx
+++ src/components/pages/MyPage/BookmarkListPage/BookmarkListPage.tsx
@@ -15,39 +15,40 @@ import ErrorText from "components/common/ErrorText/ErrorText";
 import Spinner from "components/common/Spinner/Spinner";
 import StoreList from "components/common/StoreList/StoreList";
 import Text from "components/common/Text/Text";
+import { useEffect } from "react";
+import { MESSAGES } from "constants/messages";
 
 function BookmarkListPage() {
   const navigate = useNavigate();
 
-  const { data, isLoading, isFetching, isError, error } = useQuery(
-    "bookmarkStore",
-    () => fetchBookmarkList(),
-    {
-      retry: NETWORK.RETRY_COUNT,
-      refetchOnWindowFocus: false,
-    }
-  );
+  const { data, isLoading, isFetching, isError, error } = useQuery("bookmarkStore", fetchBookmarkList, {
+    retry: 0,
+    refetchOnWindowFocus: false,
+  });
 
   const bookmarkedStoreData = data ?? [];
+
+  useEffect(() => {
+    if (error instanceof Error && error.message === MESSAGES.LOGIN_RETRY) {
+      alert(error.message);
+    }
+
+    window.location.href = PATHNAME.HOME;
+  }, [error]);
 
   return (
     <S.Container>
       <S.HeaderWrapper>
         <LeftIcon onClick={() => navigate(-1)} />
         <Text css={S.headerStyle}>나의 맛집</Text>
-        <Button
-          css={S.headerButtonStyle}
-          variant="textButton"
-          onClick={() => navigate(PATHNAME.BOOKMARK_MAP_PAGE)}
-        >
+        <Button css={S.headerButtonStyle} variant="textButton" onClick={() => navigate(PATHNAME.BOOKMARK_MAP_PAGE)}>
           지도
         </Button>
       </S.HeaderWrapper>
       {(isLoading || isFetching) && <Spinner />}
-      {isError && error instanceof Error && (
+      {isError && error instanceof Error ? (
         <ErrorImage errorMessage={error.message} />
-      )}
-      {bookmarkedStoreData.length > 0 ? (
+      ) : bookmarkedStoreData.length > 0 ? (
         <StoreList stores={bookmarkedStoreData} />
       ) : (
         <ErrorText>가게 정보가 없습니다.</ErrorText>
