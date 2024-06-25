0e95b0408d6c31c670b9f6c22ba9b1de7cba2cc8
손진영 <sm099@naver.com>
2024-06-25 16:44:55 +0900

    refactor(StoreReviewItem): 음식점 상세 페이지 내 리뷰 삭제 시 엑세스 토큰이 없는 경우의 ui 피드백 방식 변경
    window.alert를 보여주는 대신 toast를 보여주는 형태로 변경
    
    1. deleteReviewItem 내 throw new Error로 변경
    
    2. StoreReviewItem 내 deleteMutation의 onError 콜백에 showToast 추가

--- src/api/review/deleteReviewItem.tsx
+++ src/api/review/deleteReviewItem.tsx
@@ -16,9 +16,8 @@ const deleteReviewItem = async ({
   const accessToken = window.sessionStorage.getItem(ACCESS_TOKEN);
   if (!accessToken) {
     window.sessionStorage.removeItem(ACCESS_TOKEN);
-    window.alert("다시 로그인 해주세요");
-    window.location.reload();
-    return;
+
+    throw new Error("다시 로그인 해주세요.");
   }
 
   const { data } = await axiosInstance.delete<AxiosResponse>(
@@ -27,7 +26,7 @@ const deleteReviewItem = async ({
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-    }
+    },
   );
   return data;
 };
