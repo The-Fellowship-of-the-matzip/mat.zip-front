10bdb9e504e4ebcc0f0e5171d2479c0faa0a1c9a
손진영 <sm099@naver.com>
2024-06-25 17:55:54 +0900

    feat: LogoutModal 추가

---
+++ src/components/layout/Header/LogoutModal/LogoutModal.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import useLogin from "hooks/useLogin";
+
+import Modal from "components/common/Modal/Modal";
+import Button from "components/common/Button/Button";
+import Heading from "components/common/Heading/Heading";
+
+import { MESSAGES } from "constants/messages";
+import { PATHNAME } from "constants/routes";
+
+import * as S from "./LogoutModal.styled";
+import { useToastContext } from "components/common/Toast/provider/ToastProvider";
+
+interface LogoutModalProps {
+  onCloseModal: () => void;
+}
+
+export default function LogoutModal({ onCloseModal }: LogoutModalProps) {
+  const navigate = useNavigate();
+  const { logout } = useLogin();
+
+  const showToast = useToastContext();
+
+  const handleLogout = () => {
+    logout();
+    showToast(MESSAGES.LOGOUT_COMPLETE, "active");
+    onCloseModal();
+    navigate(PATHNAME.HOME);
+  };
+  return (
+    <Modal onCloseModal={onCloseModal}>
+      <Modal.ModalHeader>
+        <Heading size="xs">{MESSAGES.LOGOUT_CONFIRM}</Heading>
+        <Modal.CloseButton onCloseModal={onCloseModal} />
+      </Modal.ModalHeader>
+      <Modal.ModalContent css={S.ModalContentStyling}>
+        <p>로그아웃 시 다시 로그인 해야 합니다.</p>
+      </Modal.ModalContent>
+      <Modal.ModalFooter css={S.ButtonContainerStyling}>
+        <Button onClick={onCloseModal}>취소</Button>
+        <Button variant="primary" onClick={handleLogout}>
+          확인
+        </Button>
+      </Modal.ModalFooter>
+    </Modal>
+  );
+}
