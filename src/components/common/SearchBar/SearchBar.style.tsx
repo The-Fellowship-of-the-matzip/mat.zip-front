6cf4ed3b8ee6b96ef0140f2795b88a3d419d281a
anttiey <hanuuny@gmail.com>
2024-06-25 16:43:02 +0900

    refactor: 기존 검색창 스타일 변경

--- src/components/common/SearchBar/SearchBar.style.tsx
+++ src/components/common/SearchBar/SearchBar.style.tsx
@@ -19,15 +19,33 @@ export const InputContainer = styled.div`
 
 export const inputStyle = css`
   width: 100%;
+  height: 4.85rem;
+
+  background-color: ${({ theme }) => theme.color.primaryLight3};
+
+  border: 1px solid ${({ theme }) => theme.color.primaryLight2};
+  border-right: 0px;
   border-top-right-radius: 0;
+  border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 `;
 
 export const buttonStyle = css`
   width: 4.85rem;
   height: 4.85rem;
+
+  background-color: ${({ theme }) => theme.color.primaryLight3};
+
+  border: 1px solid ${({ theme }) => theme.color.primaryLight2};
+  border-left: 0px;
   border-top-left-radius: 0;
   border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+
+  &:hover:enabled {
+    background-color: ${({ theme }) => theme.color.primaryLight3};
+    border-color: ${({ theme }) => theme.color.primaryLight2};
+  }
 
   &:focus {
     box-shadow: none;
