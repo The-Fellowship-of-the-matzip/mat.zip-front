98c9a84d9dbbe5b4817e97aa9d444d6cbe430cdd
hwinkr <dnddl8280@naver.com>
2024-06-27 23:39:12 +0900

    chore: vite 환경에서 svg 파일을 사용하기 위한 export 경로 설정

--- src/asset/categoryIcons.ts
+++ src/asset/categoryIcons.ts
@@ -1,8 +1,8 @@
-export { ReactComponent as KoreanCategoryIcon } from "./korean-category-icon.svg";
-export { ReactComponent as ChineseCategoryIcon } from "./chinese-category-icon.svg";
-export { ReactComponent as JapaneseCategoryIcon } from "./japanese-category-icon.svg";
-export { ReactComponent as WesternCategoryIcon } from "./western-category-icon.svg";
-export { ReactComponent as SaladCategoryIcon } from "./salad-category-icon.svg";
-export { ReactComponent as MeatCategoryIcon } from "./meat-category-icon.svg";
-export { ReactComponent as CafeCategoryIcon } from "./cafe-category-icon.svg";
-export { ReactComponent as AlcoholCategoryIcon } from "./alcohol-category-icon.svg";
+export { default as KoreanCategoryIcon } from "./korean-category-icon.svg?react";
+export { default as ChineseCategoryIcon } from "./chinese-category-icon.svg?react";
+export { default as JapaneseCategoryIcon } from "./japanese-category-icon.svg?react";
+export { default as WesternCategoryIcon } from "./western-category-icon.svg?react";
+export { default as SaladCategoryIcon } from "./salad-category-icon.svg?react";
+export { default as MeatCategoryIcon } from "./meat-category-icon.svg?react";
+export { default as CafeCategoryIcon } from "./cafe-category-icon.svg?react";
+export { default as AlcoholCategoryIcon } from "./alcohol-category-icon.svg?react";
