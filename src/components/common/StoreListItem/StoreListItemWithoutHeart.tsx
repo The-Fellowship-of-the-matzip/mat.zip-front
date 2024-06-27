64225b81ed3bebca6601fb9c76d0612d5c17ca8e
hwinkr <dnddl8280@naver.com>
2024-06-27 15:42:36 +0900

    feat: 하트 UI가 없는 식당 아이템 컴포넌트 구현

---
+++ src/components/common/StoreListItem/StoreListItemWithoutHeart.tsx
@@ -0,0 +1,55 @@
+import Text from "../Text/Text";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import type { BookmarkStore } from "types/common";
+import { getRandomEmptyReviewMessage } from "util/randomUtils";
+
+import { PATHNAME } from "constants/routes";
+
+import { campusContext } from "context/CampusContextProvider";
+
+import Star from "components/common/Star/Star";
+import * as S from "components/common/StoreListItem/StoreListItem.style";
+
+function StoreListItemWithoutHeart({
+  id,
+  name,
+  distance,
+  rating,
+  reviewCount,
+  thumbnailUrl,
+}: Omit<BookmarkStore, "address">) {
+  const navigate = useNavigate();
+  const campusName = useContext(campusContext);
+
+  return (
+    <S.ListItemContainer
+      onClick={() => {
+        navigate(`${PATHNAME.STORE_DETAIL}/${id}`);
+      }}
+    >
+      <S.ListItemThumbnail src={thumbnailUrl} alt={name} />
+      <S.ListItemTextContainer>
+        <S.ListItemName>{name}</S.ListItemName>
+        <S.ListItemStars>
+          {reviewCount !== 0 ? (
+            <>
+              <Star size="sm" isFilled />
+              <Text css={S.ratingTextStyle}>{rating.toFixed(1)}</Text>
+            </>
+          ) : (
+            <>
+              <Star size="sm" />
+              <Text css={S.subTextStyle}>{getRandomEmptyReviewMessage()}</Text>
+            </>
+          )}
+        </S.ListItemStars>
+        <Text size="sm" css={S.subTextStyle}>
+          {campusName} 캠퍼스 기준 도보 {distance}분
+        </Text>
+      </S.ListItemTextContainer>
+    </S.ListItemContainer>
+  );
+}
+
+export default StoreListItemWithoutHeart;
